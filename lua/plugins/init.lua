1d200db2fc64f9d55140c8458c2d157ad28e83c0
abstractthink81 <connorsian27@gmail.com>
2025-03-20 22:00:02 -0500

    added leetcode.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -205,6 +205,27 @@ local builtin_plugins = {
       end,
       dependencies = { "copilot.lua" },
     },
+    -- Leetcode integration
+    {
+      "kawre/leetcode.nvim",
+      build = ":TSUpdate html", -- only works if you have `nvim-treesitter` installed
+      dependencies = {
+        "nvim-telescope/telescope.nvim",
+        -- "ibhagwan/fzf-lua",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+      },
+      opts = {
+        -- configuration goes here
+        ---@type lc.lang
+        lang = "c",
+        cn = { -- leetcode.cn}
+          enabled = false, ---@type boolean
+          translator = true, ---@type boolean
+          translate_problems = true, ---@type boolean
+        },
+      },
+    },
 }
 
 local exist, custom = pcall(require, "custom")
